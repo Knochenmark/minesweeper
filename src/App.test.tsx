e9f282d08c89a772a7c5b587767f17edcc717a70
knochenmark <marktodd.hl@gmail.com>
2019-03-21 20:56:23 +0100

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-// import MineSweeper from './mine-sweeper';
+import MineSweeper from './mine-sweeper';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<MineSweeper />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MineSweeper rows={6} columns={6} mines={6} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
