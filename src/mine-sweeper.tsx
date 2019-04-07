42343a0529add5490669078a739aa51ae0e5f79b
Mark Todd <marktodd.hl@gmail.com>
2019-04-07 12:21:44 +0200

    Fix gameStatus on difficulty change

--- src/mine-sweeper.tsx
+++ src/mine-sweeper.tsx
@@ -78,6 +78,7 @@ export default class MineSweeper extends React.Component<IMineSweeperProps, IMin
         rows,
         columns,
         mines,
+        gameStatus: GameStatus.PAUSED,
         mineField: this.generateMineField(rows, columns, mines),
       });
     }
