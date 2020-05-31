f6ccad14c6ee637742971c5329a87f7930a50c38
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-31 08:03:50 +0000

    Deploy Knochenmark/minesweeper to github.com/Knochenmark/minesweeper.git:gh-pages

---
+++ precache-manifest.58f6b24952c6ffd650b12dde9f365dca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47debbc7f6cc010aaff59bd38c155d9",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "66f84fb6590a2363a1f7",
+    "url": "/minesweeper/static/css/main.e79e955a.chunk.css"
+  },
+  {
+    "revision": "9dcb2728518466f42e6b",
+    "url": "/minesweeper/static/js/2.671b8f2b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/minesweeper/static/js/2.671b8f2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66f84fb6590a2363a1f7",
+    "url": "/minesweeper/static/js/main.3152eb42.chunk.js"
+  },
+  {
+    "revision": "f64bab810022455ba542",
+    "url": "/minesweeper/static/js/runtime-main.9a33a059.js"
+  },
+  {
+    "revision": "d9f529a895df9b8b997e55a03ea6acb4",
+    "url": "/minesweeper/static/media/flag.d9f529a8.svg"
+  },
+  {
+    "revision": "c9ea0a3a44a627b005a853055be8cc4e",
+    "url": "/minesweeper/static/media/mine.c9ea0a3a.svg"
+  }
+]);
